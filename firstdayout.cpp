59ebbc4534e49f63911f169828f25312c8f1976f
dmag5 <110888462+dmag5@users.noreply.github.com>
2022-08-12 16:09:30 -0400

    Add files via upload

---
+++ firstdayout.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  
+  
+ int num ;
+ int num2 ;
+ 
+ cout << "What is the first number " ; 
+ cin >> num ;
+ cout << "What is the second number " ;
+ cin >> num2 ;
+ 
+int subtraction = num - num2 ;
+cout << "The subtraction of the two numbers are " << subtraction << endl;
+
+int multiplication = num * num2 ;
+cout << "The multiplication of the numbers is " << multiplication << endl;
+
+float division = num / num2 ;
+cout << num << "/" << num2 << "=" << division ;
+
+ 
+ 
+ 
+    
+    
+    
+     
+    
+    
+
+    return 0;
+}
